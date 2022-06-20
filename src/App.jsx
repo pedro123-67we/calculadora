ec349053e9396d23d6b6f501566c4b386861a318
pedroGomez <pedrogomez5024@gmail.com>
2022-06-20 13:08:12 -0500

    feat:design changes and addition of delete button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -22,23 +22,23 @@ function App() {
       </form>
 
       <div className="teclado">
-        <button name='/' onClick={NumClick}>/</button>
+        <button class="operador" name='/' onClick={NumClick}>/</button>
         <button name='9' onClick={NumClick}>9</button>
         <button name='8' onClick={NumClick}>8</button>
         <button name='7' onClick={NumClick}>7</button>
-        <button name='X' onClick={NumClick}>X</button>
+        <button class="operador"  name='X' onClick={NumClick}>X</button>
         <button name='6' onClick={NumClick}>6</button>
         <button name='5' onClick={NumClick}>5</button>
         <button name='4' onClick={NumClick}>4</button>
-        <button name='-' onClick={NumClick}>-</button>
+        <button class="operador"  name='-' onClick={NumClick}>-</button>
         <button name='3' onClick={NumClick}>3</button>
         <button name='2' onClick={NumClick}>2</button>
         <button name='1' onClick={NumClick}>1</button>
-        <button name='+' onClick={NumClick}>+</button>
+        <button class="operador"  name='+' onClick={NumClick}>+</button>
         <button name='0' onClick={NumClick}>0</button>
         <button name='.' onClick={NumClick}>.</button>
-        <button onClick={NumClick}>=</button>
-        <button onClick={borrar} id="borrar" >Borrar</button>
+        <button class="operador"  onClick={NumClick} id="igual">=</button>
+        <button class="operador"  onClick={borrar} id="borrar" >Borrar</button>
       </div>
     </div>
 
